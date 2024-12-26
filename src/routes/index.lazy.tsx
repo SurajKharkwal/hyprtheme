dc42ad4084f93264eeb79f1cac13152c2252ebb5
SurajKharkwal <kharkwalsuraj13@gmail.com>
2024-12-26 13:50:50 +0530

    Initial Commit

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,10 @@
+import Page from "@/app/dashboard/page";
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+export const Route = createLazyFileRoute("/")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <Page />;
+}
